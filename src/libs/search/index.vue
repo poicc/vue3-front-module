2944b9a6dbd513cc1ac7c2e4c31334fa40ca455e
陈蓉琪 <chenrongqi@njbandou.com>
2022-12-25 10:58:41 +0800

    完善搜索框组件能力

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class=" group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40"
+    class="group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40"
+    ref="containerTarget"
   >
     <div>
       <!-- 搜索图标 -->
@@ -13,28 +14,129 @@
       <input
         class="block w-full h-[44px] pl-4 outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-sm text-zinc-900 tracking-wide duration-500 font-semibold border border-zinc-100 focus:border-red-300 group-hover:bg-white group-hover:border-zinc-200"
         placeholder="搜索"
+        v-model="inputValue"
+        @keyup.enter="onSearchHandler"
+        @focus="onFocusHandler"
+        @blur="onBlurHandler"
       />
       <!-- 删除按钮 -->
       <m-svg-icon
+        v-show="inputValue"
+        @click="onClearClick"
         name="input-delete"
         class="h-1.5 w-1.5 absolute translate-y-[-50%] top-[50%] right-9 cursor-pointer duration-500"
       ></m-svg-icon>
       <!-- 分割线 -->
-      <div class=" opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200 group-hover:opacity-100"></div>
+      <div
+        class="opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200 group-hover:opacity-100"
+      ></div>
       <!-- 搜索按钮 -->
-      <m-button class=" absolute translate-y-[-50%] top-[50%] right-1 rounded-full" icon="search" iconColor="#fff"></m-button>
+      <m-button
+        @click="onSearchHandler"
+        class="opacity-0 absolute translate-y-[-50%] top-[50%] right-1 rounded-full duration-500 group-hover:opacity-100"
+        icon="search"
+        iconColor="#fff"
+      ></m-button>
     </div>
     <!-- 下拉区 -->
     <transition name="slide">
-      <div class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl">
+      <div
+        v-if="$slots.dropdown"
+        v-show="isFocus"
+        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl"
+      >
         <slot name="dropdown" />
       </div>
     </transition>
   </div>
 </template>
 
+<script>
+// 双向绑定
+const EMIT_UPDATE_MODELVALUE = 'update:modelValue'
+// search搜索
+const EMIT_SEARCH = 'search'
+// 删除所有文本
+const EMIT_CLEAR = 'clear'
+// 输入事件
+const EMIT_INPUT = 'input'
+// 获取焦点事件
+const EMIT_FOCUS = 'focus'
+// 失去焦点事件
+const EMIT_BLUR = 'blur'
+
+export default {
+  name: 'search-com'
+}
+</script>
+
 <script setup>
-import { ref } from 'vue'
+import { onClickOutside, useVModel } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    required: true,
+    type: String
+  }
+})
+const emits = defineEmits([
+  EMIT_UPDATE_MODELVALUE,
+  EMIT_SEARCH,
+  EMIT_CLEAR,
+  EMIT_INPUT,
+  EMIT_FOCUS,
+  EMIT_BLUR
+])
+
+const inputValue = useVModel(props)
+/**
+ * 监听用户输入行为
+ */
+watch(inputValue, (val) => {
+  emits(EMIT_INPUT, val)
+})
+
+/**
+ * 清空文本
+ */
+const onClearClick = () => {
+  inputValue.value = ''
+  emits(EMIT_CLEAR, '')
+}
+
+/**
+ * 搜索
+ */
+const onSearchHandler = () => {
+  emits(EMIT_SEARCH, inputValue)
+}
+
+// input 是否获取到焦点
+const isFocus = ref(false)
+const containerTarget = ref(null)
+
+/**
+ * 获取到焦点
+ */
+const onFocusHandler = () => {
+  isFocus.value = true
+  emits(EMIT_FOCUS)
+}
+
+/**
+ * 失去焦点
+ */
+const onBlurHandler = () => {
+  emits(EMIT_BLUR)
+}
+
+/**
+ * 点击区域外隐藏
+ */
+onClickOutside(containerTarget, () => {
+  isFocus.value = false
+})
 </script>
 
 <style lang="scss" scoped>
