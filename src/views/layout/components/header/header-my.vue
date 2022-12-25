e072db9b096b177d0331be17e2f3110f860ae83a
陈蓉琪 <chenrongqi@njbandou.com>
2022-12-25 15:22:31 +0800

    处理用户模块

--- src/views/layout/components/header/header-my.vue
+++ src/views/layout/components/header/header-my.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>myu</div>
+  <m-popover class="flex items-center" placement="bottom-left">
+    <template #reference>
+      <div
+        class="relative flex items-center p-0.5 rounded-sm cursor-pointer duration-200 outline-none hover:bg-zinc-100"
+      >
+        <img
+          class="w-3 h-3 rounded-sm"
+          src="https://avatars.githubusercontent.com/u/59445871?v=4"
+          alt=""
+        />
+        <!-- 下箭头 -->
+        <m-svg-icon class="h-1.5 w-1.5 ml-0.5" name="down-arrow"></m-svg-icon>
+        <!-- vip -->
+        <m-svg-icon
+          class="h-1.5 w-1.5 absolute right-[16px] bottom-0"
+          name="vip"
+        ></m-svg-icon>
+      </div>
+    </template>
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in menuArr"
+        :key="item.id"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+        <span class="text-zinc-900 text-sm">{{ item.title }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+const menuArr = [
+  {
+    id: '0',
+    path: '/profile',
+    icon: 'profile',
+    title: '个人资料'
+  },
+  {
+    id: '1',
+    path: '/member',
+    icon: 'vip-profile',
+    title: '升级VIP'
+  },
+  {
+    id: '2',
+    path: '/',
+    icon: 'logout',
+    title: '退出登录'
+  }
+]
 </script>
 
 <style lang="scss" scoped></style>
