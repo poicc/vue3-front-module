a86fadb96b0e7454dc2d026ff8a6203f5aa0163f
陈蓉琪 <2560507398@qq.com>
2023-01-17 18:48:43 +0800

    ategory数据缓存  覆盖初始数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from "vuex";
 import getters from './getters'
 import category from "./modules/category";
+import createPersistedState from 'vuex-persistedstate'
 
 const state = createStore({
   getters,
   modules: {
     category
-  }
+  },
+  plugins: [
+    createPersistedState({
+      // 指定保存到 localStorage 的key
+      key: "vue3-front",
+      // 需要保存的模块
+      paths: ['category']
+    })
+  ]
 })
 
 export default state
