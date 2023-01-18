71a277330b1fda7e40a7accc40d06d4475b3faa4
陈蓉琪 <2560507398@qq.com>
2023-01-18 21:14:02 +0800

    跟随系统主题变化

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,44 @@
+import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants'
 import { watch } from 'vue'
 import store from '@/store'
-import { THEME_LIGHT, THEME_DARK } from '@/constants'
 
+/**
+ * 监听系统主题变更
+ */
+let matchMedia
+const watchSystemtThemeChange = () => {
+  // 仅需一次初始化
+  if (matchMedia) return
+  matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+  // 监听主题变化
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM)
+  }
+}
+
+/**
+ * 变更主题
+ * @param {*} theme 主题的标记
+ */
+const changeTheme = (theme) => {
+  let themeClassName = ''
+
+  switch (theme) {
+    case THEME_LIGHT:
+      themeClassName = 'light'
+      break
+    case THEME_DARK:
+      themeClassName = 'dark'
+      break
+    case THEME_SYSTEM:
+      // 调用方法 监听系统主题变化
+      watchSystemtThemeChange()
+      themeClassName = matchMedia.matches ? 'dark' : 'light'
+  }
+
+  // 修改 html 的 class
+  document.querySelector('html').className = themeClassName
+}
 /**
  * 初始化主题
  */
@@ -10,19 +47,7 @@ export default () => {
   watch(
     () => store.getters.themeType,
     (val) => {
-      let themeClassName = ''
-
-      switch (val) {
-        case THEME_LIGHT:
-          themeClassName = 'light'
-          break
-        case THEME_DARK:
-          themeClassName = 'dark'
-          break
-      }
-
-      // 修改 html 的 class
-      document.querySelector('html').className = themeClassName
+      changeTheme(val)
     },
     {
       // 初始执行一次
