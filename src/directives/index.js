5f103a61b78c79c3289c9223330f5e34587381f3
陈蓉琪 <2560507398@qq.com>
2023-01-25 21:59:44 +0800

    添加彩色占位图

--- src/directives/index.js
+++ src/directives/index.js
@@ -5,6 +5,11 @@ import lazy from './modules/lazy'
  */
 export default {
   install(app) {
-    app.directive('lazy', lazy)
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value] of Object.entries(directives)) {
+      const arr = key.split('/')
+      const name = arr[arr.length - 1].replace('.js', '')
+      app.directive(name, value.default)
+    }
   }
 }
--- src/views/main/components/list/item.vue
+++ src/views/main/components/list/item.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="bg-white dark:bg-zinc-900 xl:dark:bg-zinc-800 rounded pb-1">
-    <div class="relative w-full rounded cursor-zoom-in group">
+    <div
+      class="relative w-full rounded cursor-zoom-in group"
+      :style="{ backgroundColor: randomRGB() }"
+    >
       <!-- 图片 -->
       <img
         v-lazy
@@ -53,6 +56,7 @@
 </template>
 
 <script setup>
+import { randomRGB } from '@/utils/color'
 defineProps({
   data: {
     type: Object,
