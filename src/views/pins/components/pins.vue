7f8f4f5127f8d558e05367bf953a3aad14410c5a
陈蓉琪 <2560507398@qq.com>
2023-02-08 20:29:34 +0800

    构建navbar组件

--- src/views/pins/components/pins.vue
+++ src/views/pins/components/pins.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class=" fixed top-0 left-0 w-screen h-screen text-xl bg-slate-200 z-20">
+  <div class="fixed top-0 left-0 w-screen h-screen text-xl bg-slate-200 z-20">
     {{ id }}
-    </div>
+    <m-navbar>测试</m-navbar>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue" 
+import { ref } from 'vue'
 defineProps({
   id: {
     type: String,
     required: true
   }
 })
-
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
