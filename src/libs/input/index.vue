33413e7d1f8b0292e3229d1d92e03872916c3e2d
陈蓉琪 <2560507398@qq.com>
2023-02-21 16:21:31 +0800

    input组件构建

---
+++ src/libs/input/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="relative leading-[0px]">
+    <!-- 单行 -->
+    <input
+      v-if="type === TYPE_TEXT"
+      class="border-gray-200 dark:border-zinc-600 dark:bg-zinc-800 duration-100 dark:text-zinc-400 border outline-0 py-0.5 px-1 text-sm rounded-sm focus:border-blue-400 w-full"
+      type="text"
+      v-model="text"
+      :maxlength="max"
+    />
+    <!-- 多行 -->
+    <textarea
+      v-if="type === TYPE_TEXTAREA"
+      class="border-gray-200 dark:border-zinc-600 dark:bg-zinc-800 duration-100 dark:text-zinc-400 border outline-0 py-0.5 px-1 text-sm rounded-sm focus:border-blue-400 w-full"
+      type="text"
+      rows="5"
+      v-model="text"
+      :maxlength="max"
+    ></textarea>
+    <!-- 最大长度 -->
+    <span
+      v-if="max"
+      class="absolute right-1 bottom-0.5 text-zinc-400 text-xs"
+      :class="{ 'text-red-700': currentNumber === parseInt(max) }"
+      >{{ currentNumber / max }}</span
+    >
+  </div>
+</template>
+
+<script>
+// 单行
+const TYPE_TEXT = 'text'
+// 多行
+const TYPE_TEXTAREA = 'textarea'
+</script>
+
+<script setup>
+import { useVModel } from '@vueuse/core'
+import { ref, computed } from 'vue'
+const props = defineProps({
+  // v-model
+  modelValue: {
+    type: String,
+    required: true
+  },
+  // type
+  type: {
+    type: String,
+    default: TYPE_TEXT,
+    validator(value) {
+      const arr = [TYPE_TEXT, TYPE_TEXTAREA]
+      const result = arr.includes(value)
+      if (!result) {
+        throw new Error(`type 的值必须在可选范围内 [${arr.join('、')}]`)
+      }
+      return result
+    }
+  },
+  // max
+  max: {
+    type: [String, Number]
+  }
+})
+
+defineEmits(['update:modelValue'])
+
+// 输入的字符
+const text = useVModel(props)
+
+// 输入的字符数
+const currentNumber = computed(() => {
+  return text.value.length
+})
+</script>
+
+<style lang="scss" scoped></style>
