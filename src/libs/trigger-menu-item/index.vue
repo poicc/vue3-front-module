0e92a66609d99898c6538acd81b51de49cbbdf59
陈蓉琪 <2560507398@qq.com>
2023-02-25 15:59:27 +0800

    triier-menu组件封装

---
+++ src/libs/trigger-menu-item/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="w-5 flex flex-col items-center justify-center mx-0.5"
+    @click="onItemClick"
+  >
+    <m-svg-icon :name="icon" :fillClas="iconClass" class="w-2 h-2">
+    </m-svg-icon>
+    <p class="text-sm mt-0.5" :class="textClass">
+      <slot />
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  iconClass: {
+    type: String
+  },
+  textClass: {
+    type: String,
+    default: 'text-zinc-900'
+  },
+  to: {
+    type: String
+  }
+})
+
+const router = useRouter()
+const onItemClick = () => {
+  if (!props.to) {
+    return
+  }
+  router.push(props.to)
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="h-full overflow-auto bg-whited dark:bg-zinc-800 duration-500 scrollbar-thin scrollbar-thumb-transparent xl:scrollbar-thumb-zinc-200 xl:dark:scrollbar-thumb-zinc-900 scrollbar-track-transparent">
+  <div
+    class="h-full overflow-auto bg-whited dark:bg-zinc-800 duration-500 scrollbar-thin scrollbar-thumb-transparent xl:scrollbar-thumb-zinc-200 xl:dark:scrollbar-thumb-zinc-900 scrollbar-track-transparent"
+  >
     <navigation-vue />
-    <div class=" max-w-screen-xl mx-auto relative m-1 xl:mt-4">
+    <div class="max-w-screen-xl mx-auto relative m-1 xl:mt-4">
       <list-vue></list-vue>
     </div>
+
+    <!-- 移动端下的 tabbar -->
+    <m-trigger-menu
+      v-if="isMobileTerminal"
+      class="fixed bottom-6 m-auto left-0 right-0 w-[220px]"
+    >
+      <m-trigger-menu-item
+        icon="home"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+        >首页</m-trigger-menu-item
+      >
+      <m-trigger-menu-item
+        v-if="$store.getters.token"
+        icon="vip"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onVipClick"
+        >VIP</m-trigger-menu-item
+      >
+      <m-trigger-menu-item
+        icon="profile"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onVipClick"
+        >{{ $store.getters.token ? '我的' : '去登录' }}</m-trigger-menu-item
+      >
+    </m-trigger-menu>
   </div>
 </template>
 
 <script setup>
 import navigationVue from './components/navigation/index.vue'
 import listVue from './components/list/index.vue'
+import { isMobileTerminal } from '@/utils/flexible'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const onVipClick = () => {}
 </script>
 
 <style lang="scss" scoped></style>
