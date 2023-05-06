72e91c17977106cd5910afadb0abb8f50f7899ae
crq <59445871+poicc@users.noreply.github.com>
2023-05-06 10:30:44 +0800

    Update index.js

--- packages/docs/docs/.vitepress/theme/index.js
+++ packages/docs/docs/.vitepress/theme/index.js
@@ -5,32 +5,32 @@ import 'poicc-components/es/style.css';
 // import '../../../styles/index.scss'
 import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue';
 import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue';
-// import {
-//     PoiButton,
-//     PoiCountDown,
-//     PoiDialog,
-//     PoiInfinite,
-//     PoiInput,
-//     PoiNavbar,
-//     PoiPopover,
-//     PoiPopup,
-//     PoiSearch,
-//     PoiTriggerMenu,
-//     PoiTriggerMenuItem,
-//     PoiWaterfull,
-// } from 'poicc-components';
+import {
+    PoiButton,
+    PoiCountDown,
+    PoiDialog,
+    PoiInfinite,
+    PoiInput,
+    PoiNavbar,
+    PoiPopover,
+    PoiPopup,
+    PoiSearch,
+    PoiTriggerMenu,
+    PoiTriggerMenuItem,
+    PoiWaterfull,
+} from 'poicc-components';
 
-import PoiButton  from 'poicc-components/src/button/index.vue';
-import PoiCountDown  from 'poicc-components/src/count-down/index.vue';
-import PoiDialog  from 'poicc-components/src/dialog/index.vue';
-import PoiInfinite  from 'poicc-components/src/infinite/index.vue';
-import PoiInput  from 'poicc-components/src/input/index.vue';
-import PoiNavbar  from 'poicc-components/src/navbar/index.vue';
-import PoiPopover  from 'poicc-components/src/popover/index.vue';
-import PoiPopup  from 'poicc-components/src/popup/index.vue';
-import PoiSearch  from 'poicc-components/src/search/index.vue';
-import PoiTriggerMenu  from 'poicc-components/src/trigger-menu/index.vue';
-import PoiTriggerMenuItem  from 'poicc-components/src/trigger-menu-item/index.vue';
+// import PoiButton  from 'poicc-components/src/button/index.vue';
+// import PoiCountDown  from 'poicc-components/src/count-down/index.vue';
+// import PoiDialog  from 'poicc-components/src/dialog/index.vue';
+// import PoiInfinite  from 'poicc-components/src/infinite/index.vue';
+// import PoiInput  from 'poicc-components/src/input/index.vue';
+// import PoiNavbar  from 'poicc-components/src/navbar/index.vue';
+// import PoiPopover  from 'poicc-components/src/popover/index.vue';
+// import PoiPopup  from 'poicc-components/src/popup/index.vue';
+// import PoiSearch  from 'poicc-components/src/search/index.vue';
+// import PoiTriggerMenu  from 'poicc-components/src/trigger-menu/index.vue';
+// import PoiTriggerMenuItem  from 'poicc-components/src/trigger-menu-item/index.vue';
 // mport PoiWaterfull  from 'poicc-components/src/waterfull/index.vue';
 // import PoiButton  from '../../../../components/src/button';
 
